8643585efc0964e4dee7a5284f59c641ff8efb92
jazzycodes1215 <jaylinm1215@gmail.com>
2023-05-23 15:21:25 -0400

    Finished Functionality

--- index.js
+++ index.js
@@ -1,4 +1,26 @@
+let myNodelist = document.getElementsByTagName("LI");
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
 
+let list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
+let close = document.getElementsByClassName("close");
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
 
 const newTask = () => {
     const taskInput = document.getElementById('taskInput').value;
@@ -6,9 +28,22 @@ const newTask = () => {
     const item = document.createTextNode(taskInput)
     li.appendChild(item);
     if (taskInput === '') {
-        alert("You must write something!");
+        alert("Your Resolve Is Nonexistent");
       } else {
         document.getElementById("myUL").appendChild(li);
       }
       document.getElementById("taskInput").value = "";
+
+      var span = document.createElement("SPAN");
+      var txt = document.createTextNode("\u00D7");
+      span.className = "close";
+      span.appendChild(txt);
+      li.appendChild(span);
+    
+      for (i = 0; i < close.length; i++) {
+        close[i].onclick = function() {
+          var div = this.parentElement;
+          div.style.display = "none";
+        }
+      }
 }
